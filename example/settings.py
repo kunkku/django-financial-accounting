45018fc1470068df250848543d28eb56d0efa021
Kaarle Ritvanen <kaarle.ritvanen@datakunkku.fi>
2022-06-08 17:41:59 +0300

    example: use accounting app

--- example/settings.py
+++ example/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounting',
+    'mptt',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
